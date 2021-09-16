5600f6fe2f505c8f63abbf06e38d6e4e166dd184
pakpaki94 <danile9422@gamil.com>
2021-09-16 14:19:53 +0900

    test2

---
+++ src/main/java/com/example/study/controller/BoardController.java
@@ -0,0 +1,42 @@
+package com.example.study.controller;
+
+import com.example.study.entity.Board;
+import com.example.study.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String BoardWriteForm() {
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list",boardService.boardList());
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+}
